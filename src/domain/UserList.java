6ae2902296e74d3b29109d26e9922fbad1b9bd53
Anta <nemanjaanto@gmail.com>
2022-02-25 12:43:38 +0100

    CheckLogin interface added

--- src/domain/UserList.java
+++ src/domain/UserList.java
@@ -2,12 +2,16 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import service.LoginCheckInterface;
+import service.*;
 
-public class UserList {
+
+public class UserList implements LoginCheckInterface  {
 
 	public boolean contains(String username) {
 		// TODO Auto-generated method stub
 		return false;
-	}
+	}	
 
+ 
 }
