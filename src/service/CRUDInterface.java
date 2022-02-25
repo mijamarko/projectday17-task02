1b6625f8893a7593d21094113660af6ffce032f5
Marko Mijajlović <marko.b.mijajlovic@gmail.com>
2022-02-25 11:42:29 +0100

    added additional crud methods

--- src/service/CRUDInterface.java
+++ src/service/CRUDInterface.java
@@ -1,11 +1,12 @@
 package service;
 
-import entity.User;
+import domain.User;
 
 public interface CRUDInterface {
 
 	public void listUsers();
 	public User deleteUser(String username);
 	public User showUser(String username);
-	public User deletetUser(String username);
+	public User editUser(String username);
+	public User addUser(User user);
 }
