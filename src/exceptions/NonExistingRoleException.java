cfe56428e127d3d4e82147e2de1dd4cb2438a9c9
Marko Mijajlović <marko.b.mijajlovic@gmail.com>
2022-02-27 16:11:04 +0100

    new exception

---
+++ src/exceptions/NonExistingRoleException.java
@@ -0,0 +1,23 @@
+package exceptions;
+
+public class NonExistingRoleException extends Throwable {
+	private static final long serialVersionUID = 5L;
+
+	public NonExistingRoleException() {
+		super();
+	}
+
+	public NonExistingRoleException(String message) {
+		super(message);
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage();
+	}
+
+	@Override
+	public void printStackTrace() {
+		super.printStackTrace();
+	}
+}
